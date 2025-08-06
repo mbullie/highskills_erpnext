05fab5f12c4f836cab54f630b2150f00a281f898
michael bulwick <michael@highskills.co.il>
2025-08-06 15:55:41 +0300

    Quotation and Customer email field

--- highskills_erpnext/api.py
+++ highskills_erpnext/api.py
@@ -7,7 +7,12 @@ def sign_quotation_api(quotation_name, signature_image_base64):
         quotation = frappe.get_doc("Quotation", quotation_name)
 
         # CRITICAL Security Check
-        if frappe.session.user == "Guest" or quotation.customer_email != frappe.session.user:
+        customer_email = getattr(quotation, "contact_email", None)
+        if not customer_email and getattr(quotation, "customer", None):
+            customer_doc = frappe.get_doc("Customer", quotation.customer)
+            customer_email = getattr(customer_doc, "email_id", None)
+
+        if frappe.session.user == "Guest" or not customer_email or customer_email != frappe.session.user:
             frappe.throw("You are not authorized to sign this quotation.", frappe.PermissionError)
 
         # Save the Base64 signature as an image file
