cc1b1950cf1e77dc8e4e2705f79aa8012a0f9acb
michael bulwick <michael@highskills.co.il>
2025-08-10 16:15:33 +0300

    try to perform redirect base on request

--- highskills_erpnext/www/sign_quotation.py
+++ highskills_erpnext/www/sign_quotation.py
@@ -24,7 +24,7 @@ def get_context(context):
         quotation = frappe.get_doc("Quotation", quotation_name)
         context.quotation = quotation
         context.title = f"Sign Quotation: {quotation.name}"
-        print(f"quotation.customer_name{quotation.customer_name} quotation.customer{quotation.customer} quotation.contact_display{quotation.contact_display} quotation.contact_email {quotation.contact_email}" , flush=True)
+        print(f"quotation.customer_name {quotation.customer_name} quotation.contact_person {quotation.contact_person} quotation.contact_display {quotation.contact_display} quotation.contact_email {quotation.contact_email}" , flush=True)
     except frappe.DoesNotExistError:
         context.error_message = f"Quotation {quotation_name} not found."
         context.quotation = None
