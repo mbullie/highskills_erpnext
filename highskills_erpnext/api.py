5c1b9cbbaab0c086e16ad02d6ee71cc84d71d84b
michael bulwick <michael@highskills.co.il>
2025-08-13 11:11:54 +0300

    Added additional fields to signature

--- highskills_erpnext/api.py
+++ highskills_erpnext/api.py
@@ -1,7 +1,7 @@
 import frappe
 
 @frappe.whitelist(allow_guest=True)
-def sign_quotation_api(quotation_name, signature_image_base64):
+def sign_quotation_api(quotation_name, signature_image_base64, company_name, company_id, role_in_company, customer_name):
     try:
         quotation = frappe.get_doc("Quotation", quotation_name)
 
@@ -12,6 +12,34 @@ def sign_quotation_api(quotation_name, signature_image_base64):
         quotation.db_set("status", "Ordered") 
 
         frappe.db.commit()
+
+        # Update company 
+        if not frappe.db.exists("Company", company_name):
+            company = frappe.get_doc(
+				{
+					"doctype": "Company",
+					"company_name": company_name,
+					"country": "Israel",
+					"default_currency": "ILS",
+				}
+			)
+            company = company.save()
+        
+        company = frappe.get_doc("Company", company_name)
+
+        company.db_set("registration_details", company_id)
+        quotation.db_set("company", company_name) 
+
+        frappe.db.commit()
+
+        # Update company and personal information
+        customer = frappe.get_doc("Customer", quotation.party_name)
+
+        customer.db_set("customer_details", role_in_company)
+        customer.db_set("customer_name", customer_name)
+
+        frappe.db.commit()
+
         return {"success": True, "redirect_url": "/quotations/" + quotation_name}
 
     except frappe.PermissionError:
