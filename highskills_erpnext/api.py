ee3449292eb8a069a49c52d700b19ee7a1f4e0fc
michael bulwick <michael@highskills.co.il>
2025-11-20 12:30:52 +0200

    When signing quotation use save instead of set db to trigger events

--- highskills_erpnext/api.py
+++ highskills_erpnext/api.py
@@ -4,15 +4,20 @@ import frappe
 def sign_quotation_api(quotation_name, signature_image_base64, company_name, company_id, role_in_company, customer_name):
     try:
         quotation = frappe.get_doc("Quotation", quotation_name)
-
         # Update the quotation with the new signature and status
-        #quotation.db_set("custom_signature", signature_image_base64.split("base64,")[1])
-        quotation.db_set("custom_signature", signature_image_base64)
+        # store only the base64 payload if a data URI was provided
+        if signature_image_base64 and "base64," in signature_image_base64:
+            signature_payload = signature_image_base64.split("base64,")[1]
+        else:
+            signature_payload = signature_image_base64
+
+        quotation.custom_signature = signature_payload
         # "Ordered" is the status for signed quotations
-        quotation.db_set("status", "Ordered") 
+        quotation.status = "Ordered"
+        quotation.company = company_name
 
-        quotation.db_set("company", company_name) 
-
+        # use save() so document events / notifications (Value Change) are triggered
+        quotation.save(ignore_permissions=True)
         frappe.db.commit()
 
         # Update company 
@@ -37,9 +42,11 @@ def sign_quotation_api(quotation_name, signature_image_base64, company_name, com
         # Update company and personal information
         customer = frappe.get_doc("Customer", quotation.party_name)
 
-        customer.db_set("customer_details", "Role in company: "+ role_in_company + ",Company name: " + company_name + ",Company ID: " + company_id)
-        customer.db_set("customer_name", customer_name)
+        customer.customer_details = "Role in company: " + role_in_company + ",Company name: " + company_name + ",Company ID: " + company_id
+        customer.customer_name = customer_name
 
+        # save so any customer doc events also run
+        customer.save(ignore_permissions=True)
         frappe.db.commit()
 
         return {"success": True, "redirect_url": "/quotations/" + quotation_name}
